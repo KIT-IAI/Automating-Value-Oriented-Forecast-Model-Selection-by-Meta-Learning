dd62e5e08ac81d790a3eeafeb5f587d1e72938f3
Dorina Werling <dorina.werling@kit.edu>
2024-01-03 10:18:11 +0100

    Updated classification file

--- classification.py
+++ classification.py
@@ -1,24 +1,17 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import datetime
-import time
 
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.metrics import f1_score
+from xgboost import XGBClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
-from sklearn.gaussian_process import GaussianProcessClassifier
-from sklearn.gaussian_process.kernels import RBF
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.naive_bayes import GaussianNB
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
-from sklearn.metrics import f1_score, accuracy_score
-from xgboost import XGBClassifier
 
-
-from statsmodels.tsa.seasonal import seasonal_decompose
 
 
 ###Import PV and load data
